bd8cfc10a1492fb396f6675c1a9d5f965c0076b1
10susumu <s.i.19921029@gmail.com>
2020-07-18 21:22:55 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -14,6 +15,7 @@ class MessagesController < ApplicationController
       render :index
     end
   end
+
 
 
 
